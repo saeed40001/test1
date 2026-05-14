f51149e121f76cdd59e32bc20e6af36df04f8af1
Saeed40001 <saeedtirex3000@gmail.com>
2026-05-14 14:18:25 +0330

    Delete tests directory

--- tests/test_with_pytest.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import version
-
-from minimal_template import __version__
-
-
-def test_package_version_matches_metadata():
-    assert version("minimal_template") == __version__
